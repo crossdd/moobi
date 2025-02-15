5156269f7d1342d6daab41502aaf72841a21da7d
Epiphanus <onyesoepiphanus@gmail.com>
2025-02-15 03:29:51 +0100

    added some vercel anylytics tools

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
+
 export const metadata: Metadata = {
   title: "Epiphanus Onyeso",
   description: "Frontend web developer based in Nigeria",
@@ -25,6 +28,8 @@ export default function RootLayout({
     <html lang="en">
       <body className="antialiased">
         {children}
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
